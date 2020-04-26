f2e994e120e421717c20a6cbb76c12d5d633e8fe
Agnesxxw <Agnes_xxw@outlook.com>
2020-04-26 23:33:57 +0800

    header styling

--- takeoutApp/src/components/Header/Header.vue
+++ takeoutApp/src/components/Header/Header.vue
@@ -2,7 +2,19 @@
   <div class="header">
     <!--顶部通栏-->
     <div class="top-wrapper">
+      <div class="back-wrapper">
+        <span class="icon-arrow_lift"></span>
+      </div>
 
+      <form class="search-wrapper">
+        <span class="search-icon"></span>
+        <input class="search-bar" type="text" placeholder="Please enter items">
+      </form>
+
+      <div class="more-wrapper">
+        <a class="spelling-bt" href="#">Spell List</a>
+        <div class="more-bt"></div>
+      </div>
     </div>
     <!--主要内容-->
     <div class="content-wrapper">
@@ -10,7 +22,7 @@
     </div>
     <!--公告内容-->
     <div class="bulletin-wrapper">
-      {{poiInfo.name}}
+
     </div>
 
   </div>
@@ -28,5 +40,6 @@
 </script>
 
 <style>
+  @import url("../../common/styles/icon.css");
   @import url("head.css");
 </style>
